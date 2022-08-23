e083dbb89490e5f6c6e4aa7bd8f1df66ff4dbeec
AFreul kaium Joy <afreuljoy@gmail.com>
2022-08-23 13:26:52 +0600

    dbms connect

--- src/Components/CartDetails/CartDetails.js
+++ src/Components/CartDetails/CartDetails.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CartProducts from "../CartProducts/CartProducts";
-import fakeData from "../../fakeData";
 
 const CartDetails = () => {
-  const [products, setProducts] = useState(fakeData);
+  const [products, setProducts] = useState([]);
   
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+}, [])
 
   return (
     <div className="cartDetails-Container">
--- src/Components/CartProducts/CartProducts.js
+++ src/Components/CartProducts/CartProducts.js
@@ -3,14 +3,14 @@ import './CartProducts.css'
 
 const CartProducts = (props) => {
   console.log(props.product);
-  const { name, description, img, id,price } = props.product;
+  const { name, description, img, _id,price } = props.product;
   return (
     <div className="cartProduct-container">
       <div className="">
         <img className="img-fluid" src={img} alt="" />
       </div>
       <div>
-        <h5> ID : {id}</h5>
+        <h5> ID : {_id}</h5>
         <h3> {name}</h3>
         <p> {description}</p>
         <p> {price}</p>
